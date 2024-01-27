6b3a8f2fe92d2d57cef1c7518ec9a44f68b27351
Malcolm Seyd <malcolm@seyd.ca>
2024-01-27 13:52:09 -0600

    Remove unused bullshit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,9 @@
-import React, { useState } from "react"
 import "./App.css"
 import { CalendarView } from "./components/CalendarView"
 import { TaskView } from "./components/TaskView"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/tabs"
 
 function App() {
-  const [tabIndex, setTabIndex] = useState(0)
-
-  const handleTabClick = (event: React.SyntheticEvent, newValue: number) => {
-    setTabIndex(newValue)
-  }
-
   return (
     <div className="App">
       <Tabs defaultValue="calendar" className="w-full">
