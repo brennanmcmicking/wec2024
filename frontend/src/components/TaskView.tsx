6d1e61fa1740ca1ef1d2288c25aa14a3eae90a9f
Anika Bookout <anbookout@gmail.com>
2024-01-27 13:27:00 -0800

    Progress on task view

--- frontend/src/components/TaskView.tsx
+++ frontend/src/components/TaskView.tsx
@@ -1,36 +1,62 @@
-import { TaskEntry } from "../types"
+import { store } from "../store"
+import { RecurranceRule, TaskEntry } from "../types"
 import { Separator } from "./separator"
+import { observer } from "mobx-react-lite"
 
-interface TaskCardProps {}
-const TaskCard = (props: TaskCardProps) => {
-  return <div className="p-4 outline outline-1 outline-task"></div>
+interface TaskCardProps {
+  task: TaskEntry
 }
+const TaskCard = (props: TaskCardProps) => (
+  <div className="p-4 outline outline-1 outline-task">
+    <p className="text-lg font-semibold text-neutral-900">{props.task.title}</p>
+    <p className="text-sm text-neutral-800">{props.task.deadline.format()}</p>
+  </div>
+)
 
 interface TaskListProps {
   tasks: TaskEntry[]
   header?: string
 }
-const TaskList = (props: TaskListProps) => (
-  <div className="overflow-y-scroll">
-    {props.tasks.map((task) => (
-      <>
-        {props.header && (
-          <>
-            <h2>{props.header}</h2>
-            <Separator />
-          </>
-        )}
-        <TaskCard key={task.id} />
-      </>
-    ))}
-  </div>
-)
+const TaskList = (props: TaskListProps) => {
+  if (props.tasks.length === 0) {
+    return null
+  }
 
-interface TaskViewProps {}
-export const TaskView = (props: TaskViewProps) => {
-  let hasDailyTasks = false
-  let hasWeeklyTasks = false
-  let hasMonthlyTasks = false
+  return (
+    <div className="overflow-y-scroll">
+      {props.tasks.map((task) => (
+        <>
+          {props.header && (
+            <>
+              <h2>{props.header}</h2>
+              <Separator />
+            </>
+          )}
+          <TaskCard key={task.id} task={task} />
+        </>
+      ))}
+    </div>
+  )
+}
+
+export const TaskView = observer(() => {
+  let dailyTasks: TaskEntry[] = store.tasks.filter(
+    (task) => task.reoccuranceRule === RecurranceRule.DAILY,
+  )
+  let weeklyTasks: TaskEntry[] = store.tasks.filter(
+    (task) => task.reoccuranceRule === RecurranceRule.WEEKLY,
+  )
+  let monthlyTasks: TaskEntry[] = store.tasks.filter(
+    (task) => task.reoccuranceRule === RecurranceRule.MONTHLY,
+  )
+
+  let nonRepeatingTasks: TaskEntry[] = store.tasks.filter(
+    (task) => task.reoccuranceRule === undefined,
+  )
+
+  let hasDailyTasks = dailyTasks.length > 0
+  let hasWeeklyTasks = weeklyTasks.length > 0
+  let hasMonthlyTasks = monthlyTasks.length > 0
 
   let noRepeatingTasks = !hasDailyTasks && !hasWeeklyTasks && !hasMonthlyTasks
 
@@ -46,9 +72,9 @@ export const TaskView = (props: TaskViewProps) => {
           </div>
         ) : (
           <>
-            {hasDailyTasks && <TaskList tasks={[]} header="Daily" />}
-            {hasWeeklyTasks && <TaskList tasks={[]} header="Weekly" />}
-            {hasMonthlyTasks && <TaskList tasks={[]} header="Monthly" />}
+            <TaskList tasks={dailyTasks} header="Daily" />
+            <TaskList tasks={weeklyTasks} header="Weekly" />
+            <TaskList tasks={monthlyTasks} header="Monthly" />
           </>
         )}
       </div>
@@ -56,8 +82,8 @@ export const TaskView = (props: TaskViewProps) => {
       <div>
         {/* recurring, one-off, single, unique */}
         <h1>Unique Tasks</h1>
-        <TaskList tasks={[]} />
+        <TaskList tasks={nonRepeatingTasks} />
       </div>
     </div>
   )
-}
+})
