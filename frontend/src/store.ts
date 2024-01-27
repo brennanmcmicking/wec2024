e64148a91c38b31e5c2bed0f98d5e75a67c6748f
Malcolm Seyd <malcolm@seyd.ca>
2024-01-27 14:54:56 -0600

    serialize and deserialize moment

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,5 +1,6 @@
 import { autorun, makeAutoObservable } from "mobx"
 import { GlobalState } from "./types"
+import moment from "moment"
 
 const LOCAL_STORAGE_STATE_KEY = "MICHEALS-ANGELS-STORE"
 
@@ -14,14 +15,57 @@ const DEFAULT_STATE: GlobalState = {
   },
 }
 
+function serialize(state: GlobalState): unknown {
+  return {
+    ...state,
+    events: [
+      state.events.map((event) => ({
+        ...event,
+        start: event.start.toISOString(),
+        end: event.end.toISOString(),
+      })),
+    ],
+    tasks: [
+      state.tasks.map((task) => ({
+        ...task,
+        deadline: task.deadline.toISOString(),
+        durationEstimate: task.durationEstimate.toISOString(),
+      })),
+    ],
+  }
+}
+
+function deserialize(raw: any): GlobalState {
+  return {
+    ...raw,
+    events: [
+      raw.events.map((event: any) => ({
+        ...event,
+        start: moment(event.start),
+        end: moment(event.end),
+      })),
+    ],
+    tasks: [
+      raw.tasks.map((task: any) => ({
+        ...task,
+        deadline: moment(task.deadline),
+        durationEstimate: moment.duration(task.durationEstimate),
+      })),
+    ],
+  }
+}
+
 function getGlobalStore(): GlobalState {
   const item = window.localStorage.getItem(LOCAL_STORAGE_STATE_KEY)
   if (item === null) return DEFAULT_STATE
-  return JSON.parse(item)
+  return deserialize(JSON.parse(item))
 }
 
 export const store = makeAutoObservable(getGlobalStore())
 
 autorun(() =>
-  window.localStorage.setItem(LOCAL_STORAGE_STATE_KEY, JSON.stringify(store)),
+  window.localStorage.setItem(
+    LOCAL_STORAGE_STATE_KEY,
+    JSON.stringify(serialize(store)),
+  ),
 )
