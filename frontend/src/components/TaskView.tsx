2bb9d489461dbe52e4d31fdfc34bddc5d07ab562
Anika Bookout <anbookout@gmail.com>
2024-01-27 12:05:23 -0800

    Blocking out task view

--- frontend/src/components/TaskView.tsx
+++ frontend/src/components/TaskView.tsx
@@ -1,7 +1,23 @@
-import ViewProps from "./ViewProps"
+interface TaskCardProps {}
+const TaskCard = (props: TaskCardProps) => {
+  return <div className="p-4 outline outline-1 outline-task"></div>
+}
 
-export interface TaskViewProps extends ViewProps {}
-
+interface TaskViewProps {}
 export const TaskView = (props: TaskViewProps) => {
-  return <></>
+  let hasDailyTasks = false
+  let hasWeeklyTasks = false
+  let hasMonthlyTasks = false
+
+  return (
+    <div>
+      <div>
+        <h1>Recurring Tasks</h1>
+      </div>
+      <div>
+        {/* recurring, one-off, single, unique */}
+        <h1>Unique Tasks</h1>
+      </div>
+    </div>
+  )
 }
