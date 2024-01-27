f0246e624b91062bdae0dacb1f85cb8e5732e2a4
Malcolm Seyd <malcolm@seyd.ca>
2024-01-27 13:51:44 -0600

    Add store

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,9 @@ import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 
+import { store } from "./store"
+;(window as any).store = store
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
---
+++ frontend/src/store.ts
@@ -0,0 +1,27 @@
+import { autorun, makeAutoObservable } from "mobx"
+import { GlobalState } from "./types"
+
+const LOCAL_STORAGE_STATE_KEY = "MICHEALS-ANGELS-STORE"
+
+const DEFAULT_STATE: GlobalState = {
+  events: [],
+  tasks: [],
+  config: {
+    workingHours: {
+      start: 9,
+      end: 12 + 5,
+    },
+  },
+}
+
+function getGlobalStore(): GlobalState {
+  const item = window.localStorage.getItem(LOCAL_STORAGE_STATE_KEY)
+  if (item === null) return DEFAULT_STATE
+  return JSON.parse(item)
+}
+
+export const store = makeAutoObservable(getGlobalStore())
+
+autorun(() =>
+  window.localStorage.setItem(LOCAL_STORAGE_STATE_KEY, JSON.stringify(store)),
+)
