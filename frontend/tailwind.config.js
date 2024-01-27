ad1ba6aef1a19f99181a5353b31d691f54829bf0
Anika Bookout <anbookout@gmail.com>
2024-01-27 12:05:39 -0800

    Added temporary theme colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,13 +2,17 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
+    colors: {
+      calendar: "#0000FF",
+      task: "#FF0000",
+    },
     container: {
       center: true,
       padding: "2rem",
