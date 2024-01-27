60c440d4f9f46d0ddc6afc1af90704c2ada86f1f
Anika Bookout <anbookout@gmail.com>
2024-01-27 12:05:49 -0800

    autoformatter

--- frontend/src/components/card.tsx
+++ frontend/src/components/card.tsx
@@ -7,7 +7,7 @@ const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElemen
     <div
       ref={ref}
       className={cn(
-        "rounded-lg border border-slate-200 bg-white text-slate-950 shadow-sm dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50",
+        "border-slate-200 bg-white text-slate-950 dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50 rounded-lg border shadow-sm",
         className,
       )}
       {...props}
@@ -46,7 +46,7 @@ const CardDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <p
     ref={ref}
-    className={cn("text-sm text-slate-500 dark:text-slate-400", className)}
+    className={cn("text-slate-500 dark:text-slate-400 text-sm", className)}
     {...props}
   />
 ))
