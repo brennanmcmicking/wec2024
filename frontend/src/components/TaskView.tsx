06e91044eebbd8277d4e07909aac73d40387f2b9
Anika Bookout <anbookout@gmail.com>
2024-01-27 13:03:24 -0800

    improved Task view

--- frontend/src/components/TaskView.tsx
+++ frontend/src/components/TaskView.tsx
@@ -1,7 +1,30 @@
+import { TaskEntry } from "../types"
+import { Separator } from "./separator"
+
 interface TaskCardProps {}
 const TaskCard = (props: TaskCardProps) => {
   return <div className="p-4 outline outline-1 outline-task"></div>
 }
+
+interface TaskListProps {
+  tasks: TaskEntry[]
+  header?: string
+}
+const TaskList = (props: TaskListProps) => (
+  <div className="overflow-y-scroll">
+    {props.tasks.map((task) => (
+      <>
+        {props.header && (
+          <>
+            <h2>{props.header}</h2>
+            <Separator />
+          </>
+        )}
+        <TaskCard key={task.id} />
+      </>
+    ))}
+  </div>
+)
 
 interface TaskViewProps {}
 export const TaskView = (props: TaskViewProps) => {
@@ -17,15 +40,23 @@ export const TaskView = (props: TaskViewProps) => {
         <h1>Recurring Tasks</h1>
         {noRepeatingTasks ? (
           <div className="flex flex-col items-center justify-center p-10">
-            <p className="text-2xl font-semibold text-center">
+            <p className="text-center text-2xl font-semibold text-neutral-900">
               No recurring tasks
             </p>
           </div>
-        ) : () }
+        ) : (
+          <>
+            {hasDailyTasks && <TaskList tasks={[]} header="Daily" />}
+            {hasWeeklyTasks && <TaskList tasks={[]} header="Weekly" />}
+            {hasMonthlyTasks && <TaskList tasks={[]} header="Monthly" />}
+          </>
+        )}
       </div>
+      <Separator />
       <div>
         {/* recurring, one-off, single, unique */}
         <h1>Unique Tasks</h1>
+        <TaskList tasks={[]} />
       </div>
     </div>
   )
